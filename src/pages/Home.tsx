e260841740622230df5c6069aa6cc80a9eec3766
Patou250 <ahm.salama@gmail.com>
2025-09-17 11:15:59 +0200

    Update Home.tsx with new design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,11 @@
 import { Link } from 'react-router-dom'
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <img src="/logo.png" alt="Patou" className="w-32 h-32 mb-8" />
-      <h1 className="text-2xl font-bold mb-6">Bienvenue sur Patou</h1>
-      <div className="flex gap-6">
-        <Link
-          to="/parent/login"
-          className="px-6 py-3 rounded-lg bg-black text-white font-medium"
-        >
-          Espace Parent
-        </Link>
-        <Link
-          to="/child/login"
-          className="px-6 py-3 rounded-lg bg-green-600 text-white font-medium"
-        >
-          Espace Enfant
-        </Link>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-extrabold mb-6">Patou</h1>
+      <div className="flex gap-4">
+        <Link to="/parent/login" className="px-6 py-3 bg-black text-white rounded">Espace parent</Link>
+        <Link to="/child/login" className="px-6 py-3 bg-green-600 text-white rounded">Espace enfant</Link>
       </div>
     </div>
   )
