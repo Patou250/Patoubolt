e668ab6ab01819c58b20e276659269ef60d82304
Patou250 <ahm.salama@gmail.com>
2025-09-19 10:20:22 +0200

    Replace AppShell.tsx with simplified version

--- src/components/ui/AppShell.tsx
+++ src/components/ui/AppShell.tsx
@@ -1,21 +1,8 @@
-import { ReactNode } from 'react'
-import Header from './Header'
-import Footer from './Footer'
-
-interface AppShellProps {
-  children: ReactNode
-}
-
-export default function AppShell({ children }: AppShellProps) {
+export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header />
-      <main className="flex-1">
-        <div className="mx-auto max-w-[1200px] px-4 md:px-8 py-6">
-          {children}
-        </div>
-      </main>
-      <Footer />
+    <div className="min-h-screen bg-gray-50 text-gray-900">
+      {children}
+      {/* Footer global simple si besoin */}
     </div>
   )
 }
